3df37d223407076987b108417c26991028f6c622
Colin <colin.brown167@gmail.com>
2019-08-21 14:34:02 -0400

    first commit

---
+++ test/test-pages.js
@@ -0,0 +1,10 @@
+var expect  = require('chai').expect;
+const controller = require("../controller");
+
+it('Main page content', function(done) {
+var x = "Hello World"
+var y = controller.hello()
+    //request('http://localhost:8080' , function(error, response, body) {
+        expect(x).to.equal(y);
+        done();
+    });
