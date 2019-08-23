c5e4c054a4da86048c9e5e92b90e885015ab15f4
Colin <colin.brown167@gmail.com>
2019-08-23 14:55:25 -0400

    test passed

--- test/test-pages.js
+++ test/test-pages.js
@@ -2,7 +2,7 @@ var expect  = require('chai').expect;
 const controller = require("../controller");
 
 it('Main page content', function(done) {
-var x = "Hello World 2.0"
+var x = "Hello World 3.0"
 var y = controller.hello()
     //request('http://localhost:8080' , function(error, response, body) {
         expect(x).to.equal(y);
